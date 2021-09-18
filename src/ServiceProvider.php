562ed4e1b86f82dc7445e84d574011add14b6d0b
Shisha <shishatw225@gmail.com>
2021-09-19 05:01:34 +0800

    First commit.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DummyTeam\Dummy;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // ...
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // ...
+    }
+}
